3d6dec1dc02f108319d3c7d6728676e35fd75f22
Maliha Tarafdar <29731754+MalihaTarafdar@users.noreply.github.com>
2024-12-10 02:54:11 -0500

    red to blue keypress animation

--- keyboards/massdrop/alt/keymaps/main/custom_animations/solid_reactive_v2.h
+++ keyboards/massdrop/alt/keymaps/main/custom_animations/solid_reactive_v2.h
@@ -4,7 +4,16 @@ RGB_MATRIX_EFFECT(SOLID_REACTIVE_V2)
 #        ifdef RGB_MATRIX_CUSTOM_EFFECT_IMPLS
 
 static HSV SOLID_REACTIVE_V2_math(HSV hsv, uint16_t offset) {
-    hsv.h += scale8(255 - offset, 64); // (255 - offset) * (64 / 256)
+    hsv.h = 0;
+    hsv.s = 255;
+    hsv.v = 255;
+
+    hsv.h += scale8(255 - offset, 128);
+    if (hsv.h > 255) {
+        hsv.h = 0;
+        hsv.s = 0;
+    }
+
     return hsv;
 }
 
