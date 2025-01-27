8f2470d728799caafcce1169612b3f13eca04007
Maliha Tarafdar <29731754+MalihaTarafdar@users.noreply.github.com>
2025-01-27 14:29:36 -0500

    Improve variation in droplets animation

--- keyboards/massdrop/alt/keymaps/main/custom_animations/droplets.h
+++ keyboards/massdrop/alt/keymaps/main/custom_animations/droplets.h
@@ -11,17 +11,14 @@ static const RGB D_RGB_KEYS = SET_RGB(RGB_KEYS_R, RGB_KEYS_G, RGB_KEYS_B);
 static const RGB D_RGB_STRIP = SET_RGB(RGB_STRIP_R, RGB_STRIP_G, RGB_STRIP_B);
 static const RGB D_RGB_PRESS = SET_RGB(RGB_PRESS_R, RGB_PRESS_G, RGB_PRESS_B);
 
-#define DROPLET_CHANCE_CONSTANT 16 // (DROPLET_CHANCE_CONSTANT / 256) chance of a droplet starting on a key
-#define ANIMATION_STEPS 64
-#define VARIATION_CONSTANT 1 // number of cycles to perform at start of animation to increase variation
+#define DROPLET_CHANCE_CONSTANT 8 // (DROPLET_CHANCE_CONSTANT / 256) chance of a droplet starting on a key
+#define ANIMATION_STEPS 128 // cannot be larger than 128
 
 static void start_new_droplets(void) {
-    // start new breathing cycles
     for (uint8_t col = 0; col < MATRIX_COLS; col++) {
         for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-            // only start new cycle if key isn't already animating
             if (g_rgb_frame_buffer[row][col] == 0 && random8() < 256 / DROPLET_CHANCE_CONSTANT) {
-                g_rgb_frame_buffer[row][col] = 1; // start animation at step 1
+                g_rgb_frame_buffer[row][col] = 1;
             }
         }
     }
@@ -47,12 +44,12 @@ bool DROPLETS(effect_params_t* params) {
 
     if (params->init) {
         rgb_matrix_set_color_all(0, 0, 0);
-        memset(g_rgb_frame_buffer, 0, sizeof(g_rgb_frame_buffer));
-
-		// fast forward to increase variation at start of animation
-        for (uint8_t n = 0; n < 2 * VARIATION_CONSTANT * ANIMATION_STEPS; n++) {
-            start_new_droplets();
-			update_droplets();
+        // randomly set g_rgb_frame_buffer to increase variation
+        for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+            for (uint8_t col = 0; col < MATRIX_COLS; col++) {
+                // upperbound is exclusive, but need to use 16-bit function to avoid overflow on argument
+                g_rgb_frame_buffer[row][col] = (uint8_t) random16_max(ANIMATION_STEPS * 2);
+            }
         }
     }
 
@@ -60,8 +57,8 @@ bool DROPLETS(effect_params_t* params) {
     if (++tick > tick_speed) {
         tick = 0;
 
+        update_droplets();
         start_new_droplets();
-		update_droplets();
     }
 
     // render keys
